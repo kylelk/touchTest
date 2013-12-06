a1ce03e6c3da9f345eec5a8ac68183f8516e9974
kyle kersey <kylelk@comcast.net>
2013-12-05 16:10:50 -0800

    First commit

--- src/com/kylelk/touchtest/MainActivity.java
+++ src/com/kylelk/touchtest/MainActivity.java
@@ -13,5 +13,32 @@ public class MainActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		final TextView xCoord = (TextView) findViewById(R.id.xC);
+		final TextView yCoord = (TextView) findViewById(R.id.yC);
+		final View touchView = findViewById(R.id.touchArea);
+		
+		touchView.setOnTouchListener(new View.OnTouchListener() {
+
+				public boolean onTouch(View v, MotionEvent event) {
+					final int action = event.getAction();
+	
+					switch (action & MotionEvent.ACTION_MASK) {
+
+						case MotionEvent.ACTION_DOWN: {
+								xCoord.setText(String.valueOf((int) event.getX()));
+								yCoord.setText(String.valueOf((int) event.getY()));
+								break;
+							}
+
+						case MotionEvent.ACTION_MOVE:{
+								xCoord.setText(String.valueOf((int) event.getX()));
+								yCoord.setText(String.valueOf((int) event.getY()));
+								break;
+							}
+					}
+					return true;
+			}
+		});
     }
 }
